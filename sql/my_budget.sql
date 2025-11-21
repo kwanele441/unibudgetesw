bbcaf459a6b14c1bf3a260c67374d5e83a30eb16
Mohx <mahamulekwanele@gmail.com>
2025-11-21 21:44:14 +0200

    first upload

--- sql/my_budget.sql
+++ sql/my_budget.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 24 Maj 2020, 16:04
--- Wersja serwera: 10.4.11-MariaDB
--- Wersja PHP: 7.4.3
+-- Generation Time: Nov 21, 2025 at 08:40 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,44 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `my_budget`
+-- Database: `my_budget`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `expenses`
+-- Table structure for table `ads`
+--
+
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `discount` varchar(50) NOT NULL,
+  `valid_until` date NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_messages`
+--
+
+CREATE TABLE `contact_messages` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
 --
 
 CREATE TABLE `expenses` (
@@ -36,21 +66,29 @@ CREATE TABLE `expenses` (
   `payment_method_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `expense_comment` varchar(100) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`expense_id`, `user_id`, `expense_amount`, `expense_date`, `payment_method_id`, `category_id`, `expense_comment`) VALUES
+(7, 12, 3000.00, '2025-11-14', 1, 1, ''),
+(8, 12, 500.00, '2025-11-15', 2, 13, '');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `expense_categories`
+-- Table structure for table `expense_categories`
 --
 
 CREATE TABLE `expense_categories` (
   `category_id` int(11) NOT NULL,
   `expense_category` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `expense_categories`
+-- Dumping data for table `expense_categories`
 --
 
 INSERT INTO `expense_categories` (`category_id`, `expense_category`) VALUES
@@ -75,7 +113,26 @@ INSERT INTO `expense_categories` (`category_id`, `expense_category`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `incomes`
+-- Table structure for table `goals`
+--
+
+CREATE TABLE `goals` (
+  `goal_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `target_amount` decimal(10,2) DEFAULT NULL,
+  `target_date` date DEFAULT NULL,
+  `achieved` tinyint(1) DEFAULT 0,
+  `achieved_date` date DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incomes`
 --
 
 CREATE TABLE `incomes` (
@@ -85,21 +142,28 @@ CREATE TABLE `incomes` (
   `income_date` date NOT NULL,
   `category_id` int(11) NOT NULL,
   `income_comment` varchar(100) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `incomes`
+--
+
+INSERT INTO `incomes` (`income_id`, `user_id`, `income_amount`, `income_date`, `category_id`, `income_comment`) VALUES
+(40, 12, 5000.00, '2025-11-21', 1, '');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `income_categories`
+-- Table structure for table `income_categories`
 --
 
 CREATE TABLE `income_categories` (
   `category_id` int(11) NOT NULL,
   `income_category` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `income_categories`
+-- Dumping data for table `income_categories`
 --
 
 INSERT INTO `income_categories` (`category_id`, `income_category`) VALUES
@@ -111,16 +175,16 @@ INSERT INTO `income_categories` (`category_id`, `income_category`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `payment_methods`
+-- Table structure for table `payment_methods`
 --
 
 CREATE TABLE `payment_methods` (
   `payment_method_id` int(11) NOT NULL,
   `payment_method` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `payment_methods`
+-- Dumping data for table `payment_methods`
 --
 
 INSERT INTO `payment_methods` (`payment_method_id`, `payment_method`) VALUES
@@ -131,7 +195,7 @@ INSERT INTO `payment_methods` (`payment_method_id`, `payment_method`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -139,47 +203,108 @@ CREATE TABLE `users` (
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(12, 'mohx', 'mahamulekwanele@gmail.com', '$2y$10$7JzBiKMiD6AaK5ZRgmbv2uwMIX9qgkz1ddLMOAsUCqn342GllcSKW');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_expense_category`
+-- Table structure for table `user_expense_category`
 --
 
 CREATE TABLE `user_expense_category` (
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_expense_category`
+--
+
+INSERT INTO `user_expense_category` (`user_id`, `category_id`) VALUES
+(12, 1),
+(12, 2),
+(12, 3),
+(12, 4),
+(12, 5),
+(12, 6),
+(12, 7),
+(12, 8),
+(12, 9),
+(12, 10),
+(12, 11),
+(12, 12),
+(12, 13),
+(12, 14),
+(12, 15),
+(12, 16),
+(12, 17);
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_income_category`
+-- Table structure for table `user_income_category`
 --
 
 CREATE TABLE `user_income_category` (
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_income_category`
+--
+
+INSERT INTO `user_income_category` (`user_id`, `category_id`) VALUES
+(12, 1),
+(12, 2),
+(12, 3),
+(12, 4);
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_payment_method`
+-- Table structure for table `user_payment_method`
 --
 
 CREATE TABLE `user_payment_method` (
   `user_id` int(11) NOT NULL,
   `payment_method_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indeksy dla zrzutów tabel
+-- Dumping data for table `user_payment_method`
+--
+
+INSERT INTO `user_payment_method` (`user_id`, `payment_method_id`) VALUES
+(12, 1),
+(12, 2),
+(12, 3);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indeksy dla tabeli `expenses`
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expenses`
 --
 ALTER TABLE `expenses`
   ADD PRIMARY KEY (`expense_id`),
@@ -188,13 +313,20 @@ ALTER TABLE `expenses`
   ADD KEY `category_id` (`category_id`);
 
 --
--- Indeksy dla tabeli `expense_categories`
+-- Indexes for table `expense_categories`
 --
 ALTER TABLE `expense_categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indeksy dla tabeli `incomes`
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `incomes`
 --
 ALTER TABLE `incomes`
   ADD PRIMARY KEY (`income_id`),
@@ -202,32 +334,32 @@ ALTER TABLE `incomes`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indeksy dla tabeli `income_categories`
+-- Indexes for table `income_categories`
 --
 ALTER TABLE `income_categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indeksy dla tabeli `payment_methods`
+-- Indexes for table `payment_methods`
 --
 ALTER TABLE `payment_methods`
   ADD PRIMARY KEY (`payment_method_id`);
 
 --
--- Indeksy dla tabeli `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indeksy dla tabeli `user_expense_category`
+-- Indexes for table `user_expense_category`
 --
 ALTER TABLE `user_expense_category`
   ADD PRIMARY KEY (`user_id`,`category_id`),
   ADD KEY `category_id` (`category_id`);
 
 --
--- Indeksy dla tabeli `user_income_category`
+-- Indexes for table `user_income_category`
 --
 ALTER TABLE `user_income_category`
   ADD PRIMARY KEY (`user_id`,`category_id`),
@@ -235,7 +367,7 @@ ALTER TABLE `user_income_category`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indeksy dla tabeli `user_payment_method`
+-- Indexes for table `user_payment_method`
 --
 ALTER TABLE `user_payment_method`
   ADD PRIMARY KEY (`user_id`,`payment_method_id`),
@@ -246,47 +378,65 @@ ALTER TABLE `user_payment_method`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `expenses`
+-- AUTO_INCREMENT for table `ads`
 --
-ALTER TABLE `expenses`
-  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `ads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT dla tabeli `expense_categories`
+-- AUTO_INCREMENT for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `expense_categories`
 --
 ALTER TABLE `expense_categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT dla tabeli `incomes`
+-- AUTO_INCREMENT for table `goals`
 --
-ALTER TABLE `incomes`
-  MODIFY `income_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+ALTER TABLE `goals`
+  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT dla tabeli `income_categories`
+-- AUTO_INCREMENT for table `incomes`
+--
+ALTER TABLE `incomes`
+  MODIFY `income_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `income_categories`
 --
 ALTER TABLE `income_categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT dla tabeli `payment_methods`
+-- AUTO_INCREMENT for table `payment_methods`
 --
 ALTER TABLE `payment_methods`
   MODIFY `payment_method_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `expenses`
+-- Constraints for table `expenses`
 --
 ALTER TABLE `expenses`
   ADD CONSTRAINT `expenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
@@ -294,28 +444,34 @@ ALTER TABLE `expenses`
   ADD CONSTRAINT `expenses_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `expense_categories` (`category_id`);
 
 --
--- Ograniczenia dla tabeli `incomes`
+-- Constraints for table `goals`
+--
+ALTER TABLE `goals`
+  ADD CONSTRAINT `goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `incomes`
 --
 ALTER TABLE `incomes`
   ADD CONSTRAINT `incomes_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `income_categories` (`category_id`),
   ADD CONSTRAINT `incomes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
--- Ograniczenia dla tabeli `user_expense_category`
+-- Constraints for table `user_expense_category`
 --
 ALTER TABLE `user_expense_category`
   ADD CONSTRAINT `user_expense_category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `user_expense_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `expense_categories` (`category_id`);
 
 --
--- Ograniczenia dla tabeli `user_income_category`
+-- Constraints for table `user_income_category`
 --
 ALTER TABLE `user_income_category`
   ADD CONSTRAINT `user_income_category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `user_income_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `income_categories` (`category_id`);
 
 --
--- Ograniczenia dla tabeli `user_payment_method`
+-- Constraints for table `user_payment_method`
 --
 ALTER TABLE `user_payment_method`
   ADD CONSTRAINT `user_payment_method_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
